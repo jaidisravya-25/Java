a7b67f4d18c46ba164d31c20e5f2d86cb3730b7e
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-15 23:30:39 +0530

    Initial Commit

---
+++ tnsif/src/com/tnsif/evenodd/EvenOdd.java
@@ -0,0 +1,22 @@
+package com.tnsif.evenodd;
+
+public class EvenOdd {
+
+	
+		
+
+		
+			public static void main(String[] args) {
+		        int num = 7;  // you can change the value here
+
+		        if (num % 2 == 0) {
+		            System.out.println(num + " is Even.");
+		        } else {
+		            System.out.println(num + " is Odd.");
+		        }
+		    }
+		
+
+	}
+
+
