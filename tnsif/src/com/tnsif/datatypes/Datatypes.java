2b66fffdec820ed67e7385abefe8e7322a91a857
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-15 23:14:47 +0530

    Initial Commit

---
+++ tnsif/src/com/tnsif/datatypes/Datatypes.java
@@ -0,0 +1,47 @@
+package com.tnsif.datatypes;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+			
+			int num = 150000;
+			
+			short a = 2200;
+			
+			byte b = 127;
+			
+			long l = 20000000000L;
+			
+			float f = 3.14f;
+			
+			double d = 3.141598;
+			
+			char c = 'S';
+			
+			boolean bool = true;
+			
+			System.out.println("Integer : "+num);
+			System.out.println("Short : "+a);
+			System.out.println("Byte : "+b);
+			System.out.println("Long : "+l);
+			System.out.println("Float : "+f);
+			System.out.println("Double : "+d);
+			System.out.println("Character : "+c);
+			System.out.println("Boolean : "+bool);
+			
+			
+			String name = "Sravya";
+			
+			int[] arr = {240,241,242,243,244};
+			
+			System.out.println("String : "+name);
+			System.out.println("Array : "+arr[0]+" "+arr[1]+" "+arr[2]+" "+arr[3]+" "+arr[4]);
+			
+		
+
+	
+
+	}
+
+}
