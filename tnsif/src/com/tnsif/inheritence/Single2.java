05146514bfa8985885f80c1e38029b07a1314dc8
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-16 18:57:38 +0530

    Initial commit

---
+++ tnsif/src/com/tnsif/inheritence/Single2.java
@@ -0,0 +1,12 @@
+package com.tnsif.inheritence;
+
+public class Single2 {
+
+	public static void main(String[] args) {
+		B obj=new B();
+		obj.display();
+		System.out.println(obj.a);
+		obj.Show();
+	}
+
+}
---
+++ tnsif/src/com/tnsif/inheritence/SingleInheritence.java
@@ -0,0 +1,24 @@
+package com.tnsif.inheritence;
+
+public class SingleInheritence {
+	int a=10;
+	
+	public void Show()
+	{
+	System.out.println("This is class A");
+	}
+}
+	class B extends SingleInheritence{
+		
+	public void display()
+	{
+	System.out.println("This is class B");
+	
+	}
+	}
+
+
+
+	
+
+	
