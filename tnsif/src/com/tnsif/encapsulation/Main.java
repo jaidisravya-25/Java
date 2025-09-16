05c6809f1faa7f2549f304dc1392df3c16414e15
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-16 18:39:21 +0530

    Initial Commit

---
+++ tnsif/src/com/tnsif/encapsulation/Main.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		program p =new program();
+		System.out.println(p.getCarName());
+		System.out.println(p.getCarColor());
+		
+		
+
+	}
+
+}
---
+++ tnsif/src/com/tnsif/encapsulation/program.java
@@ -0,0 +1,19 @@
+package com.tnsif.encapsulation;
+
+public class program {
+
+	private String CarName;
+	private String CarColor;
+	public void SetCarName(String CarName) {
+		this.CarName = CarName;
+	}
+	public String getCarName() {
+		return CarName;
+	}
+	public void SetCarColor(String CarColor) {
+		this.CarColor = CarColor;
+	}
+	public String getCarColor() {
+		return CarColor;
+	}
+}
