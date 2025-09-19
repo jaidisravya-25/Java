7e92d19fd9115f7f275a4b4ea88d84a6f4d519e7
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-19 20:21:06 +0530

    Getter Setter

---
+++ tnsif/src/com/tnsif/gettersetter/GetterSetter.java
@@ -0,0 +1,30 @@
+package com.tnsif.gettersetter;
+
+class sample{
+	 private int Age;
+	 private String Name;
+	 public int getAge() {
+		 return Age;
+	 }
+	 public void setAge(int a) {
+		 Age=a;
+	 }
+	 public String getName() {
+		 return Name;
+	 }
+	 public void setName(String n) {
+		 Name = n;
+	 }
+	}
+	public class GetterSetter {
+
+		public static void main(String[] args) {
+			sample obj = new sample();
+			obj.setAge(20);
+			obj.setName("Sravya");
+			System.out.println(obj.getName()+" "+obj.getAge());
+		}
+
+	}
+
+
