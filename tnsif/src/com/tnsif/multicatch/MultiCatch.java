d8d9d1639dda89562c4f41aa4d231ea32d3c64e9
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-22 20:16:11 +0530

    Multi Catch

---
+++ tnsif/src/com/tnsif/multicatch/MultiCatch.java
@@ -0,0 +1,20 @@
+package com.tnsif.multicatch;
+
+public class MultiCatch {
+
+	public static void main(String[] args) {
+			int[] arr = new int[3];
+			try {
+				arr[3]=101;
+			}catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("This is catch block for ArrayIndexOutOfBoundsException.");
+			}
+			catch(ArithmeticException e) {
+				System.out.println("This is catch block for ArithmeticException.");
+			}
+			catch(Exception e) {
+				System.out.println("This is Excpetion occurs if not other cathc block catches it.");
+			}
+	}
+
+}
