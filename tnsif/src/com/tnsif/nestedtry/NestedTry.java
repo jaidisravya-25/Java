f15f808d9564380a18d2b866766366ca201e4d96
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-09-22 20:12:35 +0530

    Nested Try

---
+++ tnsif/src/com/tnsif/nestedtry/NestedTry.java
@@ -0,0 +1,21 @@
+package com.tnsif.nestedtry;
+
+public class NestedTry {
+
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+		try {
+			try {
+				arr[3]=101;
+			}catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("This is catch block for ArrayIndexOutOfBoundsException.");
+			}
+			int n = 10/0;
+			System.out.println(n);
+		}catch(Exception e) {
+			System.out.println("This is catch block for ArithmaticException");
+		}
+
+	}
+
+}
