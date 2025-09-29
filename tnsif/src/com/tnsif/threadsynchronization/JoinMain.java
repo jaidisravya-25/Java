91227f952dd0a250f267a3bdf6ad1223aba3f244
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7.lan>
2025-09-29 09:15:17 +0530

    Thread Synchronization

---
+++ tnsif/src/com/tnsif/threadsynchronization/JoinMain.java
@@ -0,0 +1,20 @@
+package com.tnsif.threadsynchronization;
+
+public class JoinMain {
+
+	public static void main(String[] args) {
+		for(int i =0;i<3;i++) {
+		JoinMethod obj = new JoinMethod(i);
+		Thread myThread = new Thread(obj);
+		myThread.start();
+		try {
+			myThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+
+	}
+
+}
