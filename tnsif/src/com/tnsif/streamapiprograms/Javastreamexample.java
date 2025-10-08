de001bf11e2e552959194508bd5f01c5302ecbed
Jaidi Sravya <Jaidi Sravya@LAPTOP-D4FACST7>
2025-10-08 14:41:48 +0530

    Stream API

---
+++ tnsif/src/com/tnsif/streamapiprograms/Javastreamexample.java
@@ -0,0 +1,23 @@
+package com.tnsif.streamapiprograms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Javastreamexample {
+	
+public static void main(String[] args) {
+	
+	List<Product> pl=new ArrayList<Product>();
+	
+	pl.add(new Product(1,"hp",25000f));
+	pl.add(new Product(2,"Dell",30000f));
+	pl.add(new Product(1,"Apple",95000f));
+	pl.add(new Product(1,"lenova",15000f));
+	
+	List<Float> pl2=pl.stream().filter(p->p.price>30000)//filtering data
+			.map(p->p.price)// fetching the price
+			.collect(Collectors.toList());// collecting as a list
+			System.out.println(pl2);
+}
+}
---
+++ tnsif/src/com/tnsif/streamapiprograms/Product.java
@@ -0,0 +1,16 @@
+package com.tnsif.streamapiprograms;
+
+public class Product {
+	int id;
+	String name;
+	float price;
+	
+	// parameter constructor 
+	
+	public Product(int id, String name, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+}
